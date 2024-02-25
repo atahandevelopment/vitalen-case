272159275e307806dfbf401c9235d88b61203f84
Hasan Atahan <hasan3079.hc@gmail.com>
2024-02-25 15:35:25 +0300

    ceeate

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/MovieDetail";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie-detail/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
